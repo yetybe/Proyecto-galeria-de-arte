fef653551ff443297c5dc5dba0d9428e3e829ebc
Your Name <you@example.com>
2025-08-23 21:53:37 -0400

    Entidades instanciadas /getters y setters

--- mavenproject1/src/main/java/entidades/Artista.java
+++ mavenproject1/src/main/java/entidades/Artista.java
@@ -4,11 +4,61 @@
  */
 package entidades;
 
+import java.util.ArrayList; 
+import java.util.List;
+
+
 /**
  *
  * @author Benja
  */
 public class Artista {
 
+    private int  id;
+    private String nombre;
+    private String nacionalidad;
+    private List<ObraArte> obras; 
+    
+    public Artista( int id , String nombre  , String nacionalidad )
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        obras = new ArrayList<>();
+        
+    }
+    
+    public int getId()
+    {
+        return id;
+    }
+    public  String getNombre()
+    {
+        return nombre;
+    }
+    public String getNacionalidad()
+    {
+        return nacionalidad;
+    }
+    public List<ObraArte> getObras()
+    {
+        return obras;
+    }
+    
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+    public void setNomebre(int id)
+    {
+        this.id = id;
+    }
+    public void setNacionalidad(int id)
+    {
+        this.id = id;
+    }
+    public void set
     
 }
+    
+
--- mavenproject1/src/main/java/entidades/ObraArte.java
+++ mavenproject1/src/main/java/entidades/ObraArte.java
@@ -8,7 +8,7 @@ package entidades;
  *
  * @author Benja
  */
-public class ObraArte {
+public class ObraArte{
 private String id;
 private String titulo;
 private String artista;
@@ -16,12 +16,60 @@ private int anio;
 private float precio;
 private boolean disponibilidad;
 
-public ObraArte(String codigo, String titulo, String artista, int ano, float precio, boolean disponibilidad) {
+public ObraArte(String id, String titulo, String artista, int ano, float precio, boolean disponibilidad) {
 this.id = id;
 this.titulo = titulo;
 this.artista = artista;
-this.ano = ano;
+this.anio = ano;
 this.precio = precio;
 this.disponibilidad = disponibilidad; 
+ }
+
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getTitulo() {
+	return titulo;
 }
 
+public void setTitulo(String titulo) {
+	this.titulo = titulo;
+}
+
+public String getArtista() {
+	return artista;
+}
+
+public void setArtista(String artista) {
+	this.artista = artista;
+}
+
+public int getAnio() {
+	return anio;
+}
+
+public void setAnio(int ano) {
+	this.anio = ano;
+}
+
+public float getPrecio() {
+	return precio;
+}
+
+public void setPrecio(float precio) {
+	this.precio = precio;
+}
+
+public boolean getDisponibilidad() {
+	return disponibilidad;
+}
+
+public void setDisponibilidad(boolean disponibilidad) {
+	this.disponibilidad = disponibilidad;
+}
+
+
+}
