a55db1a0673bd20914a8cca62d640b2e499b93da
Your Name <you@example.com>
2025-08-23 21:19:21 -0400

    nuevo package

---
+++ mavenproject1/src/main/java/entidades/ObraArte.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author Benja
+ */
+public class ObraArte {
+private String id;
+private String titulo;
+private String artista;
+private int anio;
+private float precio;
+private boolean disponibilidad;
+
+public ObraArte(String codigo, String titulo, String artista, int ano, float precio, boolean disponibilidad) {
+this.id = id;
+this.titulo = titulo;
+this.artista = artista;
+this.ano = ano;
+this.precio = precio;
+this.disponibilidad = disponibilidad; 
+}
+
