2460a2c94ab6c99ceb39c816c95284989c2ccc22
Your Name <you@example.com>
2025-08-26 16:41:27 -0400

    2  NUEVAS ENTIDADES ( INVENTARIO Y EXPOSICIONES) NO TERMINADAS

---
+++ mavenproject1/src/main/java/entidades/Exposiciones.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.util.ArrayList; 
+import java.util.List;
+import java.util.Date;
+
+/**
+ *
+ * @author Benja
+ */
+public class Exposicion {
+    private List<ObraArte> obrasExhibidas;
+    private String ubicacion;
+    
+    public Exposicion( int id , )
+}
