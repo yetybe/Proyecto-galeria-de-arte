ab59bd8238e726eb8cff5373242a1a9609e9d89a
Your Name <you@example.com>
2025-08-28 22:42:29 -0400

    Entidades principales terminadas / Falta ventas y prestamo

--- mavenproject1/src/main/java/entidades/Ventas.java
+++ mavenproject1/src/main/java/entidades/Ventas.java
@@ -8,6 +8,49 @@ package entidades;
  *
  * @author Benja
  */
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Ventas {
+    
+    private Map<Integer , ObraArte> obrasAlaVenta;
+    private float fondosGaleria;
+    
+    public Ventas(float fondos )
+    {
+        obrasAlaVenta = new HashMap<>();
+        fondosGaleria = 0;
+    }
+    
+    public void agregarObra(ObraArte obra) {
+    if (obra == null) return ;
+    obrasInventario.put(obra.getId(), obra); 
+    }
+    
+    public void  eliminarObra(int idObra) { 
+        obrasInventario.remove(idObra);
+    }
+    
+    public ObraArte buscarObra(int idObra) { 
+        return obrasInventario.get(idObra);
+    }
+    
+    public boolean contieneObra(int idObra) { 
+        return obrasInventario.containsKey(idObra);
+    }
+    
+    public void venderObra(int )
+    {
+        int id;
+        System.out.printf("Ingrese el id de la obra que desee vender :");
+        
+        
+        
+    }
+    
     
 }
