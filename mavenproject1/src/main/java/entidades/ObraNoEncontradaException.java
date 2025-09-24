a251d9ff3df3af6c8d3ef0f8714055793a659786
tomas <tomas@DESKTOP-SHSATBU>
2025-09-24 18:57:22 -0300

    2 clases excepcion creadas

---
+++ mavenproject1/src/main/java/entidades/ObraNoEncontradaException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author tomas
+ */
+
+public class ObraNoEncontradaException extends Exception {
+    public ObraNoEncontradaException(String mensaje) {
+        super(mensaje);
+    }
+}
