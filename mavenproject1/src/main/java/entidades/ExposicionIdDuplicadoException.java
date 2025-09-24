4c8e004c2c1068ae1ce35441fd6c310ca99274c1
tomas <tomas@DESKTOP-SHSATBU>
2025-09-24 18:57:30 -0300

    2 clases excepcion creadas

---
+++ mavenproject1/src/main/java/entidades/ExposicionIdDuplicadoException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author tomas
+ */
+public class ExposicionIdDuplicadoException extends Exception {
+    public ExposicionIdDuplicadoException(String mensaje) {
+        super(mensaje); // Llama al constructor de la clase padre (Exception)
+    }
+}
